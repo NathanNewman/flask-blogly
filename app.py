d6231b33e7f77e082c84dfe648e77370d7958d23
Nathan Newman <lordnate.moose@gmail.com>
2022-10-30 19:58:29 -0500

    completed part 2

--- app.py
+++ app.py
@@ -83,12 +83,70 @@ def add_post():
     userId = request.form['user-id']
     title = request.form['title']
     content = request.form['content']
-    # first_name = request.form['first_name']
-    # last_name = request.form['last_name']
-    # user = User.query.filter(
-    #     User.first_name == first_name, User.last_name == last_name).first()
-    # userId = user.id
     new_post = Post(title=title, content=content, user_id=userId)
     db.session.add(new_post)
     db.session.commit()
-    return posts()
+    return render_template("post-detail.html", post=new_post)
+
+
+@app.route("/edit-post_html", methods=["POST"])
+def edit_post_html():
+    postId = request.form['post-id']
+    return render_template("/edit-post.html", post=postId)
+
+
+@app.route("/edit-post", methods=["POST"])
+def edit_post():
+    postId = request.form['post-id']
+    int(postId)
+    title = request.form['title']
+    content = request.form['content']
+    post = Post.query.filter_by(id=postId).first()
+    post.title = title
+    post.content = content
+    userId = post.user_id
+    db.session.commit()
+    return user(userId)
+
+
+@app.route("/delete-post", methods=["POST"])
+def delete_post():
+    postId = request.form['post-id']
+    int(postId)
+    post = Post.query.filter_by(id=postId).first()
+    userId = post.user_id
+    Post.query.filter_by(id=postId).delete()
+    db.session.commit()
+    return user(userId)
+
+
+@app.route("/edit-user", methods=["POST"])
+def edit_user_html():
+    userId = request.form['user-id']
+    int(userId)
+    user = User.query.get(userId)
+    return render_template('/edit-user.html', user=user)
+
+
+@app.route("/editted-user", methods=["POST"])
+def edit_user():
+    userId = request.form['id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+    current_user = User.query.get(userId)
+    current_user.first_name = first_name
+    current_user.last_name = last_name
+    current_user.image_url = image_url
+    db.session.commit()
+    return user(userId)
+
+
+@app.route("/delete-user", methods=["POST"])
+def delete_user():
+    userId = request.form['user']
+    int(userId)
+    Post.query.filter_by(user_id=userId).delete()
+    User.query.filter_by(id=userId).delete()
+    db.session.commit()
+    return all_users()
